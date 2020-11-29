2fc169af50b50a143459dd0d4fd16a4cf02e6a5e
Luke Horgan <horgan.l@husky.neu.edu>
2020-11-29 16:58:32 -0500

    actually do something

--- ext/background.js
+++ ext/background.js
@@ -1,11 +1,31 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Intercept_HTTP_requests
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeSendHeaders
 function logURL(requestDetails) {
-    console.log("Loading: " + requestDetails.url);
-    if(requestDetails.url.startsWith("https://api.twitter.com/2/timeline/profile/")) {
-        console.log(requestDetails.url);
-        console.log(requestDetails);
-    }
+  //console.log("Loading: " + requestDetails.url);
+  if(requestDetails.url.startsWith("https://api.twitter.com/2/timeline/profile/")) {
+    console.log(requestDetails.url);
+    console.log(requestDetails);
+    postData("http://localhost:3040/credentials", requestDetails.requestHeaders);
+  }
+}
+
+// lifted from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
   
 browser.webRequest.onBeforeSendHeaders.addListener(
